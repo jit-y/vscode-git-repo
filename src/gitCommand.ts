c27d30ba7afed302ead00b9b480d9723f7423029
ytakhs <21127627+jit-y@users.noreply.github.com>
2021-12-07 22:40:24 +0900

    Add GitCommand class

---
+++ src/gitCommand.ts
@@ -0,0 +1,24 @@
+import * as vscode from "vscode";
+import { GitExtension } from "./@types/git";
+import { Err, Ok, Result } from "./result";
+
+export class GitCommand {
+  private readonly gitApi;
+  private readonly gitPath;
+
+  constructor(extension: vscode.Extension<GitExtension>) {
+    this.gitApi = extension.exports.getAPI(1);
+    this.gitPath = this.gitApi.git.path;
+  }
+}
+
+export function createGitCommand(): Result<GitCommand, Error> {
+  const extension = vscode.extensions.getExtension<GitExtension>("vscode.git");
+  if (extension === undefined) {
+    return Err(new Error("extension vscode.git is not found"));
+  }
+
+  const gitCommand = new GitCommand(extension);
+
+  return Ok(gitCommand);
+}
